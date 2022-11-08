f6795435ad4552b90b3da0e70f5e2577ca045341
imakero <45946863+imakero@users.noreply.github.com>
2022-11-08 12:17:16 +0100

    add jwt auth

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Req() req: Request) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }

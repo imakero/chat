f59f9d559361a172789facb257da06053f8a0cf8
imakero <45946863+imakero@users.noreply.github.com>
2022-11-08 08:59:53 +0100

    add users module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { User } from './typeorm/entities/User';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { User } from './typeorm/entities/User';
         synchronize: true,
       }),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],

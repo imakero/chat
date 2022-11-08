cb379098f3222600aad53e782e8d18c0c7408269
imakero <45946863+imakero@users.noreply.github.com>
2022-11-08 09:01:39 +0100

    add users controller

--- packages/backend/src/users/users.module.ts
+++ packages/backend/src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users.controller';
 import { User } from '../typeorm/entities/User';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
 })
 export class UsersModule {}

678d8549d1c46e14e0dd3b45e2280ac8ce6457eb
imakero <45946863+imakero@users.noreply.github.com>
2022-11-08 11:35:43 +0100

    add passport local strategy

--- packages/backend/src/auth/auth.service.ts
+++ packages/backend/src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { comparePassword } from 'src/utils/bcrypt';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(username: string, inputPassword: string): Promise<any> {
+    const user = await this.usersService.findOneByUsername(username);
+    if (user && comparePassword(inputPassword, user.password)) {
+      delete user.password;
+      return user;
+    }
+    return null;
+  }
+}

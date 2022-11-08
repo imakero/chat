27e27e9d55f23bba4be25c1348c53e285b6386e8
imakero <45946863+imakero@users.noreply.github.com>
2022-11-08 10:01:19 +0100

    feature: create user

--- packages/backend/src/users/users.controller.ts
+++ packages/backend/src/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.usersService.create(createUserDto);
+  }
+
   @Get()
   async findAll() {
     return await this.usersService.findAll();
--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { hashPassword } from '../utils/bcrypt';
 import { Repository } from 'typeorm';
 import { User } from '../typeorm/entities/User';
+import { CreateUserParams } from './types';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    private userRepository: Repository<User>,
   ) {}
 
+  async create(userDetails: CreateUserParams): Promise<User> {
+    const password = await hashPassword(userDetails.password);
+    const user = this.userRepository.create({
+      ...userDetails,
+      password,
+    });
+    return this.userRepository.save(user);
+  }
+
   findAll(): Promise<User[]> {
-    return this.usersRepository.find();
+    return this.userRepository.find();
   }
 }

5f91b9e89c21badc29a5179033b1992d4232edee
imakero <45946863+imakero@users.noreply.github.com>
2022-11-10 10:10:21 +0100

    add input component

---
+++ packages/frontend/components/form/input.tsx
@@ -0,0 +1,29 @@
+import { Field, FieldInputProps, FieldProps } from "formik";
+import {
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+
+export interface InputProps extends ChakraInputProps {
+  name: string;
+}
+
+const Input = ({ name, ...props }: InputProps) => {
+  return (
+    <Field name={name}>
+      {({ field: { value, onChange, onBlur } }: FieldProps) => {
+        return (
+          <ChakraInput
+            {...props}
+            value={value}
+            name={name}
+            onBlur={onBlur}
+            onChange={onChange}
+          />
+        );
+      }}
+    </Field>
+  );
+};
+
+export default Input;

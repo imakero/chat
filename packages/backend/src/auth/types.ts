fe425ead40d87e0efc0e084e5c1afaaa4a0042c5
imakero <45946863+imakero@users.noreply.github.com>
2022-11-10 06:50:12 +0100

    type req.user for jwt routes

--- packages/backend/src/auth/types.ts
+++ packages/backend/src/auth/types.ts
@@ -1,4 +1,13 @@
+import { Request } from 'express';
+
 export type JwtPayload = {
   sub: string;
   username: string;
 };
+
+export interface JwtRequest extends Request {
+  user: {
+    id: number;
+    username: string;
+  };
+}

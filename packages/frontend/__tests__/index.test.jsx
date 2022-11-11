35f911e341da682993252153ce54d732e44a2304
imakero <45946863+imakero@users.noreply.github.com>
2022-11-11 14:59:40 +0100

    mock event source related functions

--- packages/frontend/__tests__/index.test.jsx
+++ packages/frontend/__tests__/index.test.jsx
@@ -2,9 +2,15 @@ import { render, screen } from "@testing-library/react";
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
 import { useRouter } from "next/router";
+import { useEventSource, useEventSourceListener } from "../lib/event-source";
 
 jest.mock("next/router", () => ({
   useRouter: jest.fn(),
+}));
+
+jest.mock("../lib/event-source", () => ({
+  useEventSource: jest.fn(),
+  useEventSourceListener: jest.fn(),
 }));
 
 describe("Home", () => {
@@ -16,6 +22,9 @@ describe("Home", () => {
     asPath: "/",
     query: "",
   }));
+
+  useEventSource.mockImplementation(() => [undefined]);
+  useEventSourceListener.mockImplementation(() => {});
 
   it("renders a heading containing chat", () => {
     render(<Home />);

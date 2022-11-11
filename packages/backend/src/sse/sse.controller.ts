71aefcafbb3f466b2f6341de984f9ed9fd05dc01
imakero <45946863+imakero@users.noreply.github.com>
2022-11-11 14:26:02 +0100

    protect sse route with cookie jwt

--- packages/backend/src/sse/sse.controller.ts
+++ packages/backend/src/sse/sse.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Sse } from '@nestjs/common';
+import { Controller, Sse, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { SseService } from './sse.service';
 
 @Controller('sse')
 export class SseController {
   constructor(private readonly sseService: SseService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Sse()
   doTheSse() {
     return this.sseService.sendEvents();
